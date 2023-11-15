d9f292465cbe29896b14639c441f6910aff4e816
loxia01 <loxia_01@tutanota.com>
2023-11-15 14:48:47 +0100

    v1.1.7

--- FormatColumn.psm1
+++ FormatColumn.psm1
@@ -1,40 +1,42 @@
+using namespace System.Management.Automation
+
 function Format-Column
 {
 <#
 .SYNOPSIS
  Format-Column formats object data as columns, ordering data column by column as default.
- 
+
 .DESCRIPTION
  Format-Column function outputs object data into columns, similarly to built-in cmdlet Format-Wide.
  It can order output data column by column in addition to row by row,
  as is the only option in Format-Wide.
- 
+
 .PARAMETER Property
  Name of object property to be displayed.
- 
+
  The value of the Property parameter can also be a calculated property:
  - a hash table. Valid syntaxes are:
      - @{Expression=<string>|{<scriptblock>}}
      - @{FormatString=<string>}
      - @{Expression=<string>|{<scriptblock>}; FormatString=<string>}
-     
+
  - a script block: {<scriptblock>}
- 
+
  Property parameter is optional. However, if omitted for data containing properties,
  but missing DefaultDisplayProperty, no comprehensible data output will be produced.
- 
+
 .PARAMETER ColumnCount
  Number of columns to display (CustomSize mode). If ColumnCount parameter is omitted the number
  of columns is calculated automatically (AutoSize mode).
- 
+
 .PARAMETER MaxColumnCount
  Maximum number of columns to display in AutoSize mode. Optional.
  Cannot be combined with ColumnCount parameter.
- 
+
 .PARAMETER MinRowCount
  Minimum number of rows to display in AutoSize mode. Optional.
  Cannot be combined with ColumnCount parameter.
- 
+
 .PARAMETER GroupBy
  Formats the output in groups based on a shared property or value. Optional.
  Can be the name of a property or an expression (hash table or script block):
@@ -42,45 +44,45 @@ function Format-Column
      - @{Expression=<string>|{<scriptblock>}}
      - @{Label/Name=<string>; Expression=<string>|{<scriptblock>}}
      - @{Label/Name=<string>; Expression=<string>|{<scriptblock>}; FormatString=<string>}
- 
+
  - a script block: {<scriptblock>}
- 
+
 .PARAMETER OrderBy
  Determines data order in column output. Default value is Column.
- 
+
  Valid values are:
  - Column: Orders data column by column.
  - Row: Orders data row by row.
- 
+
 .PARAMETER InputObject
  Object to format for display. Accepts pipeline input.
- 
+
 .EXAMPLE
  1..100 | Format-Column -MinRowCount 20 -OrderBy Row
- 
+
 .EXAMPLE
  Format-Column -Property @{FormatString='{0:000}'} -ColumnCount 3 -InputObject (1..125)
- 
+
 .EXAMPLE
  Get-Process | Format-Column -Property @{Expr='Id'; FormatStr='{0:00000}'}
- 
+
 .EXAMPLE
  # The following Property syntaxes are all equivalent:
- 
+
  Get-Process | Format-Column -Property ProcessName              # name (string)
  Get-Process | Format-Column -Property {$_.ProcessName}         # scriptblock
  Get-Process | Format-Column -Property @{Expr='ProcessName'}    # hashtable string expression
  Get-Process | Format-Column -Property @{Expr={$_.ProcessName}} # hashtable scriptblock expression
- 
+
 .INPUTS
  You can pipe any object to Format-Column.
- 
+
 .OUTPUTS
  Format-Column returns strings that represent the output table.
- 
+
 .NOTES
  Included alias for Format-Column is 'fcol'.
- 
+
 .LINK
  Online version: https://github.com/loxia01/FormatColumn
 #>
@@ -90,48 +92,58 @@ function Format-Column
         [Parameter(Position=0)]
         [SupportsWildcards()]
         [Object]$Property,
-        
+
         [Parameter(ParameterSetName='CustomSize', Mandatory)]
         [ValidateScript({$_ -gt 0})]
         [int]$ColumnCount,
-        
+
         [Parameter(ParameterSetName='AutoSize')]
         [ValidateScript({$_ -gt 0})]
         [int]$MaxColumnCount,
-        
+
         [Parameter(ParameterSetName='AutoSize')]
         [ValidateScript({$_ -gt 0})]
         [int]$MinRowCount,
-        
+
         [Parameter()]
         [SupportsWildcards()]
         [Object]$GroupBy,
-        
+
         [Parameter()]
         [ValidateSet('Column','Row')]
         [string]$OrderBy = 'Column',
-        
+
         [Parameter(ValueFromPipeline)]
         [psobject]$InputObject
     )
     if ($input) { $InputObject = $input }
-    
+
     if ($null -eq $InputObject) { return }
-    
+
     # Property and GroupBy validation and processing
-    
+
     $properties = $InputObject[0].PSObject.Properties
-    
+
     if ($Property)
     {
         if ($Property -is [hashtable])
         {
             $Property.Keys | ForEach-Object {
-                if     ($_ -match '^ex?p?r?e?s?s?i?o?n?$')     { $pExpr = $Property.$_ }
-                elseif ($_ -match '^fo?r?m?a?t?s?t?r?i?n?g?$') { $pFormatStr = $Property.$_ }
-                else { Write-Error "Invalid Property key '${_}'." -Category 5 -EA 1 }
+                if     ($_ -match '^e(x(p(r(e(s(s(i(on?)?)?)?)?)?)?)?)?$')         { $pExpr = $Property.$_ }
+                elseif ($_ -match '^f(o(r(m(a(t(s(t(r(i(ng?)?)?)?)?)?)?)?)?)?)?$') { $pFormatStr = $Property.$_ }
+                else
+                {
+                    $message = "Invalid key '${_}' in Property hashtable."
+                    $errArgs = (New-Object NotSupportedException $message), 'DictionaryKeyIllegal', 5, $null
+                    $PSCmdlet.ThrowTerminatingError((New-Object ErrorRecord $errArgs))
+                }
             }
-            if ($pFormatStr -and $pFormatStr -isnot [string]) { Write-Error "Invalid Property formatstring type." -Category 5 -EA 1 }
+            if ($pFormatStr -and $pFormatStr -isnot [string])
+            {
+                $message = "Formatstring key in Property hashtable is not of type String."
+                $errArgs = (New-Object NotSupportedException $message), 'DictionaryKeyIllegalValue', 5, $null
+                $PSCmdlet.ThrowTerminatingError((New-Object ErrorRecord $errArgs))
+            }
             if ($pExpr)
             {
                 if ($pExpr -is [string])
@@ -149,9 +161,19 @@ function Format-Column
                     if ($pFormatStr) { $propertySelect = {$pFormatStr -f (& $pExpr)} }
                     else             { $propertySelect = $pExpr }
                 }
-                else { Write-Error "Invalid Property expression type." -Category 5 -EA 1 }
+                else
+                {
+                    $message = "Expression key in Property hashtable is not of type String or ScriptBlock."
+                    $errArgs = (New-Object NotSupportedException $message), 'DictionaryKeyIllegalValue', 5, $null
+                    $PSCmdlet.ThrowTerminatingError((New-Object ErrorRecord $errArgs))
+                }
             }
-            else { Write-Error "Property hash table is missing mandatory expression entry." -Category 5 -EA 1 }
+            else
+            {
+                $message = "Property hashtable is missing mandatory expression entry."
+                $errArgs = (New-Object NotSupportedException $message), 'DictionaryKeyMandatoryEntry', 5, $null
+                $PSCmdlet.ThrowTerminatingError((New-Object ErrorRecord $errArgs))
+            }
         }
         elseif ($Property -is [string])
         {
@@ -162,7 +184,12 @@ function Format-Column
             $propertySelect = {$_.$Property -join ", "}
         }
         elseif ($Property -is [scriptblock]) { $propertySelect = [scriptblock]::Create("@(${Property}) -join ', '") }
-        else { Write-Error "Invalid Property type." -Category 5 -EA 1 }
+        else
+        {
+            $message = "Property parameter value is not of type String, ScriptBlock or Hashtable."
+            $errArgs = (New-Object NotSupportedException $message), 'DictionaryKeyUnknownType', 5, $null
+            $PSCmdlet.ThrowTerminatingError((New-Object ErrorRecord $errArgs))
+        }
     }
     else
     {
@@ -183,7 +210,7 @@ function Format-Column
                     else { $InputObject[0].PSStandardMembers.DefaultDisplayPropertySet.ReferencedPropertyNames[0] }
                 }
                 else { $InputObject[0].PSStandardMembers.DefaultDisplayProperty }
-            
+
             $propertySelect = {$_.$defaultDisplayProperty -join ", "}
         }
         else
@@ -196,29 +223,44 @@ function Format-Column
                 }
                 elseif (-not ($properties.Name -match '^(Count|Length)$')) { @($properties.Name)[0] }
                 else { $false }
-            
+
             if ($displayProperty) { $propertySelect = {$_.$displayProperty -join ", "} }
             else                  { $propertySelect = {$_ -join ", "} }
-        }   
+        }
     }
-    
+
     if (-not $GroupBy)
     {
         $outputData = $InputObject | ForEach-Object $propertySelect
-        trap { Write-Error $_ -EA 1 }
+        trap { $PSCmdlet.ThrowTerminatingError($_) }
     }
     else
     {
         if ($GroupBy -is [hashtable])
         {
             $GroupBy.Keys | ForEach-Object {
-                if     ($_ -match '^(na?m?e?|la?b?e?l?)$')     { $gLabel = $GroupBy.$_ }
-                elseif ($_ -match '^ex?p?r?e?s?s?i?o?n?$')     { $gExpr = $GroupBy.$_ }
-                elseif ($_ -match '^fo?r?m?a?t?s?t?r?i?n?g?$') { $gFormatStr = $GroupBy.$_ }
-                else { Write-Error "Invalid GroupBy hash table key '${_}'." -Category 5 -EA 1 }
+                if     ($_ -match '^n(a(me?)?)?$|^l(a(b(el?)?)?)?$')               { $gLabel = $GroupBy.$_ }
+                elseif ($_ -match '^e(x(p(r(e(s(s(i(on?)?)?)?)?)?)?)?)?$')         { $gExpr = $GroupBy.$_ }
+                elseif ($_ -match '^f(o(r(m(a(t(s(t(r(i(ng?)?)?)?)?)?)?)?)?)?)?$') { $gFormatStr = $GroupBy.$_ }
+                else
+                {
+                    $message = "Invalid key '${_}' in GroupBy hashtable."
+                    $errArgs = (New-Object NotSupportedException $message), 'DictionaryKeyIllegal', 5, $null
+                    $PSCmdlet.ThrowTerminatingError((New-Object ErrorRecord $errArgs))
+                }
             }
-            if ($gLabel -and $gLabel -isnot [string]) { Write-Error "Invalid GroupBy name/label type." -Category 5 -EA 1 }
-            if ($gFormatStr -and $gFormatStr -isnot [string]) { Write-Error "Invalid GroupBy formatstring type." -Category 5 -EA 1 }
+            if ($gLabel -and $gLabel -isnot [string])
+            {
+                $message = "Label/Name key in GroupBy hashtable is not of type String."
+                $errArgs = (New-Object NotSupportedException $message), 'DictionaryKeyIllegalValue', 5, $null
+                $PSCmdlet.ThrowTerminatingError((New-Object ErrorRecord $errArgs))
+            }
+            if ($gFormatStr -and $gFormatStr -isnot [string])
+            {
+                $message = "Formatstring key in GroupBy hashtable is not of type String."
+                $errArgs = (New-Object NotSupportedException $message), 'DictionaryKeyIllegalValue', 5, $null
+                $PSCmdlet.ThrowTerminatingError((New-Object ErrorRecord $errArgs))
+            }
             if ($gExpr)
             {
                 if ($gExpr -is [string])
@@ -233,9 +275,19 @@ function Format-Column
                     if ($gFormatStr) { $groupSelect = {$gFormatStr -f (& $gExpr)} }
                     else             { $groupSelect = $gExpr }
                 }
-                else { Write-Error "Invalid GroupBy expression type." -Category 5 -EA 1 }
+                else
+                {
+                    $message = "Expression key in GroupBy hashtable is not of type String or ScriptBlock."
+                    $errArgs = (New-Object NotSupportedException $message), 'DictionaryKeyIllegalValue', 5, $null
+                    $PSCmdlet.ThrowTerminatingError((New-Object ErrorRecord $errArgs))
+                }
             }
-            else { Write-Error "GroupBy hash table is missing mandatory expression entry." -Category 5 -EA 1 }
+            else
+            {
+                $message = "GroupBy hashtable is missing mandatory expression entry."
+                $errArgs = (New-Object NotSupportedException $message), 'DictionaryKeyMandatoryEntry', 5, $null
+                $PSCmdlet.ThrowTerminatingError((New-Object ErrorRecord $errArgs))
+            }
         }
         elseif ($GroupBy -is [string])
         {
@@ -243,56 +295,61 @@ function Format-Column
             $groupSelect = {$_.$GroupBy -join ", "}
         }
         elseif ($GroupBy -is [scriptblock]) { $groupSelect = [scriptblock]::Create("@(${GroupBy}) -join ', '") }
-        else { Write-Error "Invalid GroupBy type." -Category 5 -EA 1 }
-        
+        else
+        {
+            $message = "GroupBy parameter value is not of type String, ScriptBlock or Hashtable."
+            $errArgs = (New-Object NotSupportedException $message), 'DictionaryKeyUnknownType', 5, $null
+            $PSCmdlet.ThrowTerminatingError((New-Object ErrorRecord $errArgs))
+        }
+
         if (-not $gLabel)
         {
             if ($gExpr) { $gLabel = $gExpr }
             else        { $gLabel = $GroupBy }
         }
-        
+
         $outputData = $InputObject | ForEach-Object {
             [pscustomobject]@{$propertySelect = & $propertySelect; $groupSelect = & $groupSelect}
         }
         $groupValues = $outputData.$groupSelect | Sort-Object -Unique
-        
+
         $outputDataGroups = [Collections.Generic.List[Object]]@()
         foreach ($groupValue in $groupValues)
         {
             $outputDataGroups.Add($outputData.Where({$_.$groupSelect -eq $groupValue}).$propertySelect)
         }
-        
-        trap { Write-Error $_ -EA 1 }
+
+        trap { $PSCmdlet.ThrowTerminatingError($_) }
     }
-    
+
     # Output Processing
-    
+
     if (-not $psISE) { $consoleWidth = $Host.UI.RawUI.WindowSize.Width }
     else             { $consoleWidth = $Host.UI.RawUI.BufferSize.Width }
     $columnGap = 1
-    
+
     if (-not $outputDataGroups)
     {
         $maxLength = ($outputData | Measure-Object Length -Maximum).Maximum
-        
+
         if (-not $ColumnCount)
         {
             $ColumnCount = [Math]::Max(1, [Math]::Floor($consoleWidth / ($maxLength + $columnGap)))
-            
+
             if ($outputData.Count -lt $ColumnCount) { $ColumnCount = $outputData.Count }
             if ($MaxColumnCount -and $MaxColumnCount -lt $ColumnCount) { $ColumnCount = $MaxColumnCount }
         }
-        
+
         $rowCount = [Math]::Ceiling($outputData.Count / $ColumnCount)
-        
+
         if ($MinRowCount -and $MinRowCount -gt $rowCount)
         {
             $ColumnCount = [Math]::Max(1, [Math]::Floor($outputData.Count / $MinRowCount))
             $rowCount = [Math]::Ceiling($outputData.Count / $ColumnCount)
         }
-        
+
         $columnWidth = [Math]::Floor(($consoleWidth - $ColumnCount * $columnGap) / $ColumnCount)
-        
+
         <# Truncate strings longer than column width (applicable only for CustomSize mode, or in
            AutoSize mode if string lengths greater than or equal to console width are present). #>
         if ($maxLength -gt $columnWidth)
@@ -307,7 +364,7 @@ function Format-Column
             # Write terminating error if column width is too small for truncate ellipsis "..."
             else { Write-Error "ColumnCount value too large for output display." -Category 5 -EA 1 }
         }
-        
+
         # Create format string for output
         $alignment = -($columnWidth + $columnGap)
         $formatString = (
@@ -315,9 +372,9 @@ function Format-Column
                 "{${_},${alignment}}"
             }
         ) -join ""
-        
+
         # Output data ordered column by column or row by row
-        
+
         if ($PSEdition -eq 'Desktop') { Write-Output "`n" }
         else                          { Write-Output "" }
         if ($OrderBy -eq 'Column')
@@ -352,26 +409,26 @@ function Format-Column
             $ColumnCount = ($outputDataGroups | ForEach-Object {
                 $maxLength = ($_ | Measure-Object Length -Maximum).Maximum
                 $colCount = [Math]::Max(1, [Math]::Floor($consoleWidth / ($maxLength + $columnGap)))
-                
+
                 if ($MaxColumnCount -and $MaxColumnCount -lt $colCount) { $MaxColumnCount }
                 else                                                    { $colCount }
             } | Measure-Object -Minimum).Minimum
         }
-        
+
         $i = 0
         foreach ($outputDataGroup in $outputDataGroups)
         {
             $rowCount = [Math]::Ceiling($outputDataGroup.Count / $ColumnCount)
-            
+
             if ($MinRowCount -and $MinRowCount -gt $rowCount)
             {
                 $ColumnCount = [Math]::Max(1, [Math]::Floor($outputDataGroup.Count / $MinRowCount))
                 $rowCount = [Math]::Ceiling($outputDataGroup.Count / $ColumnCount)
             }
-            
+
             $maxLength = ($outputDataGroup | Measure-Object Length -Maximum).Maximum
             $columnWidth = [Math]::Floor(($consoleWidth - $ColumnCount * $columnGap) / $ColumnCount)
-            
+
             <# Truncate strings longer than column width (applicable only for CustomSize mode, or in
                AutoSize mode if string lengths greater than or equal to console width are present). #>
             if ($maxLength -gt $columnWidth)
@@ -386,7 +443,7 @@ function Format-Column
                 # Write terminating error if column width is too small for truncate ellipsis "..."
                 else { Write-Error "ColumnCount value too large for output display." -Category 5 -EA 1 }
             }
-            
+
             # Create format string for output
             $alignment = -($columnWidth + $columnGap)
             $formatString = (
@@ -394,13 +451,13 @@ function Format-Column
                     "{${_},${alignment}}"
                 }
             ) -join ""
-            
+
             # Output data ordered column by column or row by row, adding group label and value
-            
+
             if ($PSEdition -eq 'Desktop') { Write-Output "" }
             Write-Output "`n   ${gLabel}: $(@($groupValues)[$i])`n"
             if ($PSEdition -eq 'Desktop') { Write-Output "" }
-            
+
             if ($OrderBy -eq 'Column')
             {
                 0..($rowCount - 1) | ForEach-Object {
